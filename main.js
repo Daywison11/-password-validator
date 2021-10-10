b14798f188bb97750e69606cbbca805e2c6d359a
Daywison <ddvison.11@gmail.com>
2021-10-10 13:57:23 -0300

    pagando os valores do input senha

---
+++ main.js
@@ -0,0 +1,11 @@
+function permision(){
+    verificar()
+}
+
+function verificar(){
+    let senha1 = document.querySelector('#password').value
+    let senha2= document.querySelector('#password1').value
+    let tamanho  = senha1.length
+    console.log(tamanho)
+}
+
