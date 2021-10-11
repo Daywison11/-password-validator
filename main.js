879c36870dc80a0381d439811333270a3e7255d9
Daywison <ddvison.11@gmail.com>
2021-10-11 14:11:10 -0300

    o formulario ja retorna true ou false

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function permision(){
-    removespaces()
-    countingsize()
+    senhaOk()
 }
 function arrayadd(){
     //pegando os itens da senha e colocando em um array
@@ -19,8 +18,9 @@ function removespaces(){
         let array = arrayadd()
         let espaços = array.includes(' ')
         if( espaços ==true ){
-            let res = document.querySelector("#resposta")
+            let res = document.querySelector("#space")
             res.innerHTML = 'A senha não pode conter espaços'
+            res.style.color = 'red'
 
             //removendo os  espaços da senha
             var buscar = " ";
@@ -32,7 +32,7 @@ function removespaces(){
             return true
         }
         else{
-            let res = document.querySelector("#resposta")
+            let res = document.querySelector("#space")
             res.innerHTML = ''
             return false
         } 
@@ -55,10 +55,97 @@ function countingsize(){
     if ( size <= 7 ){
         msg.innerText = 'no minimo 8 caracter'
         msg.style.color = 'red'
+        return false
     }
     else if( size > 7 ){
         msg.innerText = ''
+        return true
     }
-    console.log(size)
 }
 
+//verificando se tem alguma letra maiuscula
+//se tiver retorna true se nao false
+function haveuppercase(){
+    let array = arrayadd()
+    var senha = array.toString()
+    var regex = /[A-Z]/
+    if(!regex.exec(senha)){
+        let msg  = document.getElementById('upper')
+        msg.innerText = 'uma letra maiuscula'
+        msg.style.color = 'red'
+        return false
+    }
+    else{
+        let msg  = document.getElementById('upper')
+        msg.innerText = ''
+        return true
+    }
+    
+}
+
+//verifica se tem ao menos um  numero na senha 
+//se tiver retorna true se nao false
+function haveNumber(){
+    let msg = document.getElementById('number')
+    let array = arrayadd()
+    var senha = array.toString()
+    var regex = /[0-9]/
+    if(!regex.exec(senha)){
+        msg.innerText = 'deve conter numeros'
+        msg.style.color = 'red'
+
+        return false
+    }
+    else{
+        msg.innerText = ''
+        return true
+    }
+}
+//verifica se tem algum caractere na senha
+//caso tenha retorna true se nao false
+function haveCaracter(){
+    let msg = document.getElementById('caracter')
+    let senha = document.getElementById('password').value
+    let regex = /[-!@#$%*()_+^&}{:;?.,]/ 
+    if(!regex.exec(senha)){
+        msg.innerText = 'um caractere " =@.,*/+'
+        msg.style.color = 'red'
+        return false
+    } 
+    else{
+        msg.innerText = ''
+        return true
+    }
+   
+}
+//verifica se a senha um e dois estão iguais
+function validesenha2(){
+    let msg = document.getElementById('resposta')
+    let senha1 = document.getElementById('password').value
+    let senha2 = document.getElementById('password2').value
+    if (senha2 != senha1){
+        msg.innerText = 'verifique a confirmação de senha'
+        msg.style.color = 'red'
+        return false
+    }
+    else if (senha1 === senha2){
+        msg.innerText = ''
+        return true
+    }
+    
+}
+
+function senhaOk(){
+    let espace = removespaces()
+    let size = countingsize()
+    let upper = haveuppercase()
+    let number = haveNumber()
+    let caractere = haveCaracter()
+    let igual = validesenha2()
+    if(espace == false & size == true & upper == true & number == true & caractere == true & igual == true){
+        return true
+    }
+    else{
+        return false
+    }
+}
